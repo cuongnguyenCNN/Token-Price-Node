a7f68aaa6898029ce2551d369beac31b534e2bf2
Nguyễn Ngọc Cương <cuongnguyen71195@gmail.com>
2024-12-25 11:08:57 +0700

    OK T

--- src/binance.js
+++ src/binance.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const axios = require('axios');
 
 /**
  * Binance REST API wrapper.
@@ -32,27 +33,52 @@ module.exports = class Binance {
      * @returns {Promise} Response promise.
      */
     request(path, options) {
-        return new Promise((resolve, reject) => {
-            request('https://api.binance.com' + path, options, (err, res, body) => {
-                if (err) {
-                    return reject(err)
-                }
-                if (!body) {
-                    return reject(new Error('No body'))
-                }
+        return new Promise(async(resolve, reject) => {
+            // request('https://api.binance.com' + path, options, (err, res, body) => {
+            //     if (err) {
+            //         return reject(err)
+            //     }
+            //     if (!body) {
+            //         return reject(new Error('No body'))
+            //     }
 
-                try {
-                    const json = JSON.parse(body)
-                    if (json.code && json.msg) {
-                        const err = new Error(json.msg)
-                        err.code = json.code
-                        return reject(err)
-                    }
-                    return resolve(json)
-                } catch (err) {
-                    return reject(err)
+            //     try {
+            //         const json = JSON.parse(body)
+            //         if (json.code && json.msg) {
+            //             const err = new Error(json.msg)
+            //             err.code = json.code
+            //             return reject(err)
+            //         }
+            //         return resolve(json)
+            //     } catch (err) {
+            //         return reject(err)
+            //     }
+            // })
+            try {
+                // Build the full URL
+                const url = `https://api.binance.com${path}`;
+    
+                // Make the HTTP request with axios
+                const response = await axios({ url, ...options });
+    
+                // Check for Binance-specific error structure
+                if (response.data.code && response.data.msg) {
+                    const err = new Error(response.data.msg);
+                    err.code = response.data.code;
+                    return reject(err);
                 }
-            })
+    
+                // Resolve the Promise with parsed JSON data
+                resolve(response.data);
+            } catch (error) {
+                // Handle network errors or HTTP errors
+                if (error.response && error.response.data) {
+                    const err = new Error(error.response.data.msg || 'Request failed');
+                    err.code = error.response.data.code || error.response.status;
+                    return reject(err);
+                }
+                reject(error);
+            }
         })
     }
 }
